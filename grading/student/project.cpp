0c00a2409e1c28a3d737faa98c3ac7f2781be4c0
瘋狂挺腰獸 <s111062109@m111.nthu.edu.tw>
2024-01-16 13:01:00 +0800

    Update improve methods

--- grading/student/project.cpp
+++ grading/student/project.cpp
@@ -69,7 +69,11 @@ class Simulate{
             cache_sets = 0;
             associativity = 0;
         }
-        ~Simulate() {}
+        ~Simulate() {
+            if(correlation_matrix != NULL) {
+                delete correlation_matrix;
+            } 
+        }
         void set_address_bits(int address_bits) { this->address_bits = address_bits;}
         void set_block_size(int block_size) {
             this->block_size = block_size;
@@ -85,8 +89,11 @@ class Simulate{
         int get_block_size() { return block_size;}
         int get_cache_sets() { return cache_sets;}
         int get_associativity() { return associativity;}
-        void simulation(ofstream& outfile);
-        void initialize(ifstream& infile, ofstream& outfile, bool LSB_en = false);
+        void simulation();
+        void recursion(int bits_remain, vector<double> quality, set<int> index_bits);
+        void initialize(ifstream& infile);
+        void set_corr_matrix(ifstream& infile, ofstream& outfile);
+        void output(ofstream& outfile);
     private:
         int address_bits;
         int block_size;
@@ -97,9 +104,12 @@ class Simulate{
         int cache_bits;
         vector<string> ref;
         vector<string> tags;
-        set<int> index_bits;
+        set< set < int > > index_bits;
         string benchmark;
         map<string,Cache> simulate_cache; 
+        vector< vector< double > >* correlation_matrix;
+        int miss = 1e9;
+        set < int > best = set < int >();
 };
 
 int main(int argc, char *argv[]) {
@@ -145,13 +155,105 @@ int main(int argc, char *argv[]) {
     outfile << endl;
     infile.close();
     infile.open(argv[2]);
-    simulate.initialize(infile, outfile);
-    simulate.simulation(outfile);
+    simulate.set_corr_matrix(infile, outfile);
+    simulate.initialize(infile);
+    simulate.simulation();
+    simulate.output(outfile);
     infile.close();
     return 0;
 }
 
-void Simulate::initialize(ifstream& infile, ofstream& outfile, bool LSB_en) {
+void Simulate::initialize(ifstream& infile) {
+    vector<double> quality (cache_bits, 0);
+    for(int i = 0; i < ref.size(); i++) {
+        for(int j = 0; j < cache_bits; j++) {
+            quality[j] += (ref[i][j+offset_bits] == '1') ? 1 : 0;
+        }
+    }
+    for(int i = 0; i < cache_bits; i++) {
+        if(quality[i] > (ref.size() - quality[i])) 
+            quality[i] = (double) (ref.size() - quality[i]) / quality[i];
+        else
+            quality[i] = (double) quality[i] / (ref.size() - quality[i]);
+    }
+    recursion(index_bits_num, quality, set<int>());
+}
+
+void Simulate::recursion(int bits_remain, vector<double> quality, set<int> index_bits){
+    if(bits_remain == 0) {
+        int i = 0;
+        while (index_bits.size() < index_bits_num) {
+            index_bits.insert(i++);
+        }
+        this->index_bits.insert(index_bits);
+        return;
+    }
+    double max = 0;
+    for(int j = 0; j < cache_bits; j++) {
+        if(quality[j] > max) max = quality[j];
+    }
+    int flag = 0;
+    for(vector<double>::iterator it = quality.begin(); it != quality.end(); ++it) {
+        if(*it > 0.2){
+            flag = 1;
+            int max_index = it - quality.begin();
+            vector<double> tmp = quality;
+            for(int j = 0; j < cache_bits; j++) {
+                tmp[j] *= (*correlation_matrix)[max_index][j];
+            }
+            set<int> tmp2 = index_bits;
+            tmp2.insert(max_index);
+            recursion(bits_remain - 1, tmp, tmp2);
+        }
+    }
+    if(flag == 0) {
+        for(vector<double>::iterator it = quality.begin(); it != quality.end(); ++it) {
+            if(*it == max){
+                int max_index = it - quality.begin();
+                vector<double> tmp = quality;
+                for(int j = 0; j < cache_bits; j++) {
+                    tmp[j] *= (*correlation_matrix)[max_index][j];
+                }
+                set<int> tmp2 = index_bits;
+                tmp2.insert(max_index);
+                recursion(bits_remain - 1, tmp, tmp2);
+            }
+        }
+    }
+}
+
+void Simulate::simulation() {
+    for(set< set < int > >::iterator it = index_bits.begin(); it != index_bits.end(); ++it) {
+        int tmp_miss = 0;
+        for(int i=0; i<ref.size(); i++){
+            string address = ref[i].substr(offset_bits, address_bits - offset_bits);
+            string index = "";
+            string tag = "";
+            for(int j = 0; j < cache_bits; j++) {
+                if((*it).find(j) != (*it).end()) {
+                    index += address[j];
+                }
+                else {
+                    tag += address[j];
+                }
+            }
+            if(simulate_cache.find(index) == simulate_cache.end()) {
+                simulate_cache[index] = Cache(associativity);
+            }
+            bool hit = simulate_cache[index].hit(tag);
+            if(!hit) {
+                tmp_miss++;
+            }
+        }
+        if(tmp_miss < miss) {
+            miss = tmp_miss;
+            best = *it;
+        }
+        simulate_cache.clear();
+    }
+}
+
+void Simulate::set_corr_matrix(ifstream& infile, ofstream& outfile) {
     outfile << "Offset bit count: " << offset_bits << endl;
     outfile << "Indexing bit count: " << index_bits_num << endl;
     getline(infile, benchmark);
@@ -161,74 +263,41 @@ void Simulate::initialize(ifstream& infile, ofstream& outfile, bool LSB_en) {
         reverse(line.begin(), line.end());
         ref.push_back(line);
     }
-    if(LSB_en){
-        for(int i = 0; i < index_bits_num; i++) {
-            index_bits.insert(i);
-        }
-    }
-    else{
-        vector< vector<double> > correlation_matrix (cache_bits, vector<double>(cache_bits, 0));
-        for(int i = 0; i < ref.size(); i++) {
-            for(int j = 0; j < cache_bits; j++) {
-                for(int k = j + 1; k < cache_bits; k++) {
-                    if(ref[i][j+offset_bits] == ref[i][k+offset_bits]) {
-                        correlation_matrix[j][k]++;
-                        correlation_matrix[k][j]++;
-                    }
+    correlation_matrix = new vector< vector<double> >(cache_bits, vector<double>(cache_bits, 0));
+    for(int i = 0; i < ref.size(); i++) {
+        for(int j = 0; j < cache_bits; j++) {
+            for(int k = j + 1; k < cache_bits; k++) {
+                if(ref[i][j+offset_bits] == ref[i][k+offset_bits]) {
+                    (*correlation_matrix)[j][k]++;
+                    (*correlation_matrix)[k][j]++;
                 }
             }
         }
-        for(int i = 0; i < cache_bits; i++) {
-            for(int j = 0; j < cache_bits; j++) {
-                if(correlation_matrix[i][j] > ref.size() - correlation_matrix[i][j]) 
-                    correlation_matrix[i][j] = (double) (ref.size() - correlation_matrix[i][j]) / correlation_matrix[i][j];
-                else
-                    correlation_matrix[i][j] = (double) correlation_matrix[i][j] / (ref.size() - correlation_matrix[i][j]);
-            }
-        }
-        vector<double> quality (cache_bits, 0);
-        for(int i = 0; i < ref.size(); i++) {
-            for(int j = 0; j < cache_bits; j++) {
-                quality[j] += (ref[i][j+offset_bits] == '1') ? 1 : 0;
-            }
-        }
-        for(int i = 0; i < cache_bits; i++) {
-            if(quality[i] > (ref.size() - quality[i])) 
-                quality[i] = (double) (ref.size() - quality[i]) / quality[i];
+    }
+    for(int i = 0; i < cache_bits; i++) {
+        for(int j = 0; j < cache_bits; j++) {
+            if((*correlation_matrix)[i][j] > ref.size() - (*correlation_matrix)[i][j]) 
+                (*correlation_matrix)[i][j] = (double) (ref.size() - (*correlation_matrix)[i][j]) / (*correlation_matrix)[i][j];
             else
-                quality[i] = (double) quality[i] / (ref.size() - quality[i]);
-        }
-        for(int i=0; i<index_bits_num; i++) {
-            int max_index = 0;
-            for(int j = 0; j < cache_bits; j++) {
-                if(quality[j] > quality[max_index]) max_index = j;
-            }
-            index_bits.insert(max_index);
-            for(int j = 0; j < cache_bits; j++) {
-                quality[j] *= correlation_matrix[max_index][j];
-            }
-        }
-        int i = 0;
-        while (index_bits.size() < index_bits_num) {
-            index_bits.insert(i++);
+                (*correlation_matrix)[i][j] = (double) (*correlation_matrix)[i][j] / (ref.size() - (*correlation_matrix)[i][j]);
         }
     }
+}
+
+void Simulate::output(ofstream& outfile) {
     outfile << "Indexing bits:";
-    for(set<int>::reverse_iterator i = index_bits.rbegin(); i != index_bits.rend(); ++i) {
+    for(set<int>::iterator i = best.begin(); i != best.end(); ++i) {
         outfile << " " << *i + offset_bits;
     }
     outfile << endl << endl;
-}
-
-void Simulate::simulation(ofstream& outfile) {
-    int total_miss = 0;
     outfile << benchmark << endl;
+    int total_miss = 0;
     for(int i=0; i<ref.size(); i++){
         string address = ref[i].substr(offset_bits, address_bits - offset_bits);
         string index = "";
         string tag = "";
         for(int j = 0; j < cache_bits; j++) {
-            if(find(index_bits.begin(), index_bits.end(), j) != index_bits.end()) {
+            if(best.find(j) != best.end()) {
                 index += address[j];
             }
             else {
@@ -244,10 +313,9 @@ void Simulate::simulation(ofstream& outfile) {
             outfile << ref[i] <<  " " << "hit" << endl;
         }
         else {
-            total_miss++;
             outfile << ref[i] <<  " " << "miss" << endl;
         }
     }
     outfile << ".end" << endl << endl;
-    outfile << "Total cache miss count: " << total_miss << endl;
+    outfile << "Total cache miss count: " << miss << endl;
 }
